a49d71e98d8493ffd4014b83804345c4307dc14e
kikuuchi <laputech06@gmail.com>
2024-02-03 04:13:41 +0900

    feat: database に接続するための処理に実装

---
+++ database.py
@@ -0,0 +1,22 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+load_dotenv()
+
+import os
+
+DB_URL = 'mysql+pymysql://{}:{}@{}:{}/{}'.format(
+    os.environ['DB_USERNAME'],
+    os.environ['DB_PASSWORD'],
+    os.environ['DB_HOST'],
+    os.environ['DB_PORT'],
+    os.environ['DB_DATABASE'],
+)
+
+engine = create_engine(
+    DB_URL, connect_args={"check_same_thread": False}
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
