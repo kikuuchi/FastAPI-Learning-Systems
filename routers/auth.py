f2122092bd544102e4068ae645d570556ec3fb28
kikuuchi <laputech06@gmail.com>
2024-02-03 11:34:59 +0900

    feat: トークン作成関数の実装

--- routers/auth.py
+++ routers/auth.py
@@ -10,6 +10,18 @@ import cruds.auth as auth_crud
 
 router = APIRouter()
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+# パスワード認証を行い、トークンを生成
+def create_tokens(user_id: int):
+    # ペイロード作成
+    access_payload = {
+        'token_type': 'access_token',
+        'exp': datetime.utcnow() + timedelta(minutes=60),
+        'user_id': user_id
+    }
+    access_token = jwt.encode(access_payload, os.environ["SECRET_KEY"], algorithm='HS256')
+    return{'access_token': access_token, 'token_type': 'bearer'}
 @router.post("/login")
 async def login():
     pass
